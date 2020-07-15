5d5b6c971638a787a55f9cdc3356490d442fcdcb
Rick Martínez <rickmzp@gmail.com>
2020-07-14 23:37:53 -0400

    Very basic addition of state via hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,48 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React, { useState } from "react";
+import { render } from "react-dom";
 
-const House = () => (
+type HouseProps = {
+  piecesCount: number,
+  onPlay?: Function,
+}
+
+const House = ({ piecesCount, onPlay }: HouseProps) => (
   <td>
-    <p>4</p>
-    <p><button>Play</button></p>
+    <p>{piecesCount}</p>
+    <p><button onClick={(_event) => onPlay()}>Play</button></p>
   </td>
 )
 
-const GameBoard = () => (
-  <table style={{border: "3px solid black"}}>
-    <tr>
-      <td rowSpan={2}>0</td>
-      <House />
-      <House />
-      <House />
-      <House />
-      <House />
-      <House />
-      <td rowSpan={2}>0</td>
-    </tr>
-    <tr>
-      <House />
-      <House />
-      <House />
-      <House />
-      <House />
-      <House />
-    </tr>
-  </table>
-);
+const GameBoard = () => {
+  const [gameState, setGameState] = useState({
+    houseCount: 4,
+  });
 
-ReactDOM.render(
+  return (
+    <table style={{ border: "3px solid black" }}>
+      <tr>
+        <td rowSpan={2}>0</td>
+        <House
+          piecesCount={gameState.houseCount}
+          onPlay={() => setGameState({ houseCount: gameState.houseCount + 1 })}
+        />
+        {/* <House piecesCount={houseCount} /> */}
+        <td rowSpan={2}>0</td>
+      </tr>
+      <tr>
+        <House piecesCount={gameState.houseCount} />
+        {/* <House />
+        <House />
+        <House />
+        <House />
+        <House />
+        <House /> */}
+      </tr>
+    </table>
+  );
+};
+
+render(
   <GameBoard />,
   document.getElementById("react-root")
 );
