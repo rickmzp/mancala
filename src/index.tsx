fc503fac4b633de3d2ccc46414078e5a68609064
Rick Martínez <rickmzp@gmail.com>
2020-07-18 14:03:34 -0400

    Implement basic turn logic for player A

--- src/index.tsx
+++ src/index.tsx
@@ -13,24 +13,72 @@ const House = ({ piecesCount, onPlay }: HouseProps) => (
   </td>
 )
 
+interface GameState {
+  sideAHouses: Array<number>,
+  sideAStoreCount: number,
+  sideBHouses: Array<number>,
+  sideBStoreCount: number,
+}
+
+const generateInitialGameState = (): GameState =>  ({
+  sideAHouses: [4, 4, 4, 4, 4, 4],
+  sideAStoreCount: 0,
+  sideBHouses: [4, 4, 4, 4, 4, 4],
+  sideBStoreCount: 0,
+});
+
 const GameBoard = () => {
-  const [gameState, setGameState] = useState({
-    houseCount: 4,
-  });
+  const [gameState, setGameState] = useState(generateInitialGameState());
+
+  const playHouse = (index: number) => {
+    const currentState = gameState;
+    const updatedSideAHouses = currentState.sideAHouses;
+    const updatedSideBHouses = currentState.sideBHouses;
+    let updatedSideAStoreCount = currentState.sideAStoreCount;
+
+    const stonesCurrentlyInHouse = updatedSideAHouses[index]
+    updatedSideAHouses[index] = 0;
+    for (let i = 1; i <= stonesCurrentlyInHouse; i++) {
+      const houseToIncrement = index + i;
+      if (houseToIncrement == currentState.sideAHouses.length) {
+        updatedSideAStoreCount++;
+      } else if (houseToIncrement > currentState.sideAHouses.length) {
+        const otherSideHouseToIncrement = houseToIncrement - currentState.sideAHouses.length - 1;
+        updatedSideBHouses[otherSideHouseToIncrement] = updatedSideBHouses[otherSideHouseToIncrement] + 1;
+      } else {
+        updatedSideAHouses[houseToIncrement] = updatedSideAHouses[houseToIncrement] + 1;
+      }
+    }
+
+    const newState = {
+      ...currentState,
+      sideAHouses: updatedSideAHouses,
+      sideBHouses: updatedSideBHouses,
+      sideAStoreCount: updatedSideAStoreCount,
+    };
+    setGameState(newState);
+  }
 
   return (
     <table style={{ border: "3px solid black" }}>
       <tr>
-        <td rowSpan={2}>0</td>
-        <House
-          piecesCount={gameState.houseCount}
-          onPlay={() => setGameState({ houseCount: gameState.houseCount + 1 })}
-        />
+        <td rowSpan={2}>{gameState.sideBStoreCount}</td>
+        {gameState.sideBHouses.slice().reverse().map((count, index) => (
+          <House
+            piecesCount={count}
+            onPlay={() => playHouse(index)}
+          />
+        ))}
         {/* <House piecesCount={houseCount} /> */}
-        <td rowSpan={2}>0</td>
+        <td rowSpan={2}>{gameState.sideAStoreCount}</td>
       </tr>
       <tr>
-        <House piecesCount={gameState.houseCount} />
+        {gameState.sideAHouses.map((count, index) => (
+          <House
+            piecesCount={count}
+            onPlay={() => playHouse(index)}
+          />
+        ))}
         {/* <House />
         <House />
         <House />
