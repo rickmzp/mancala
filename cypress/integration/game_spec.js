a7c73070812467a0ff6160b0b9e56315c534e201
Rick Martínez <rickmzp@gmail.com>
2020-07-29 20:34:05 -0400

    Easier to read assertions for game state in tests

--- cypress/integration/game_spec.js
+++ cypress/integration/game_spec.js
@@ -4,21 +4,17 @@ describe('playing stones', () => {
 
     // TODO: is there a better matcher than contains?
     cy.get('.playerA-house2 button').click();
-    cy.get('.playerA-house1 .count').should('contain', '4');
-    cy.get('.playerA-house2 .count').should('contain', '0');
-    cy.get('.playerA-house3 .count').should('contain', '5');
-    cy.get('.playerA-house4 .count').should('contain', '5');
-    cy.get('.playerA-house5 .count').should('contain', '5');
-    cy.get('.playerA-house6 .count').should('contain', '5');
+    boardHousesShouldLookLikeThis(
+      [4, 4, 4, 4, 4, 4],
+      [4, 0, 5, 5, 5, 5],
+    );
     cy.get('.playerA-store').should('contain', '0');
 
     cy.get('.playerB-house6 button').click();
-    cy.get('.playerA-house1 .count').should('contain', '5');
-    cy.get('.playerA-house2 .count').should('contain', '1');
-    cy.get('.playerA-house3 .count').should('contain', '6');
-    cy.get('.playerA-house4 .count').should('contain', '5');
-    cy.get('.playerA-house5 .count').should('contain', '5');
-    cy.get('.playerA-house6 .count').should('contain', '5');
+    boardHousesShouldLookLikeThis(
+      [0, 4, 4, 4, 4, 4],
+      [5, 1, 6, 5, 5, 5],
+    );
     cy.get('.playerA-store').should('contain', '0');
   });
 
@@ -27,19 +23,11 @@ describe('playing stones', () => {
 
     // TODO: is there a better matcher than contains?
     cy.get('.playerA-house3 button').click();
-    cy.get('.playerA-house1 .count').should('contain', '4');
-    cy.get('.playerA-house2 .count').should('contain', '4');
-    cy.get('.playerA-house3 .count').should('contain', '0');
-    cy.get('.playerA-house4 .count').should('contain', '5');
-    cy.get('.playerA-house5 .count').should('contain', '5');
-    cy.get('.playerA-house6 .count').should('contain', '5');
+    boardHousesShouldLookLikeThis(
+      [4, 4, 4, 4, 4, 4],
+      [4, 4, 0, 5, 5, 5]
+    );
     cy.get('.playerA-store').should('contain', '1');
-    cy.get('.playerB-house1 .count').should('contain', '4');
-    cy.get('.playerB-house2 .count').should('contain', '4');
-    cy.get('.playerB-house3 .count').should('contain', '4');
-    cy.get('.playerB-house4 .count').should('contain', '4');
-    cy.get('.playerB-house5 .count').should('contain', '4');
-    cy.get('.playerB-house6 .count').should('contain', '4');
     cy.get('.playerB-store').should('contain', '0');
   });
 
@@ -48,19 +36,11 @@ describe('playing stones', () => {
 
     // TODO: is there a better matcher than contains?
     cy.get('.playerA-house4 button').click();
-    cy.get('.playerA-house1 .count').should('contain', '4');
-    cy.get('.playerA-house2 .count').should('contain', '4');
-    cy.get('.playerA-house3 .count').should('contain', '4');
-    cy.get('.playerA-house4 .count').should('contain', '0');
-    cy.get('.playerA-house5 .count').should('contain', '5');
-    cy.get('.playerA-house6 .count').should('contain', '5');
+    boardHousesShouldLookLikeThis(
+      [4, 4, 4, 4, 4, 5],
+      [4, 4, 4, 0, 5, 5]
+    );
     cy.get('.playerA-store').should('contain', '1');
-    cy.get('.playerB-house1 .count').should('contain', '5');
-    cy.get('.playerB-house2 .count').should('contain', '4');
-    cy.get('.playerB-house3 .count').should('contain', '4');
-    cy.get('.playerB-house4 .count').should('contain', '4');
-    cy.get('.playerB-house5 .count').should('contain', '4');
-    cy.get('.playerB-house6 .count').should('contain', '4');
     cy.get('.playerB-store').should('contain', '0');
   });
 
@@ -78,19 +58,23 @@ describe('playing stones', () => {
     cy.get('.playerB-house4 button').click();
     cy.get('.playerA-house5 button').click();
 
-    cy.get('.playerA-house1 .count').should('contain', '3');
-    cy.get('.playerA-house2 .count').should('contain', '2');
-    cy.get('.playerA-house3 .count').should('contain', '1');
-    cy.get('.playerA-house4 .count').should('contain', '1');
-    cy.get('.playerA-house5 .count').should('contain', '0');
-    cy.get('.playerA-house6 .count').should('contain', '8');
+    boardHousesShouldLookLikeThis(
+      [8, 9, 1, 2, 3, 3],
+      [3, 2, 1, 1, 0, 8]
+    );
     cy.get('.playerA-store').should('contain', '4');
-    cy.get('.playerB-house1 .count').should('contain', '3');
-    cy.get('.playerB-house2 .count').should('contain', '3');
-    cy.get('.playerB-house3 .count').should('contain', '2');
-    cy.get('.playerB-house4 .count').should('contain', '1');
-    cy.get('.playerB-house5 .count').should('contain', '9');
-    cy.get('.playerB-house6 .count').should('contain', '8');
     cy.get('.playerB-store').should('contain', '3');
   });
+
+  const boardHousesShouldLookLikeThis = (playerB, playerA) => {
+    playerB.forEach((expectedCount, index) => {
+      const number = 6 - index;
+      cy.get(`.playerB-house${number} .count`).should('contain', expectedCount);
+    });
+
+    playerA.forEach((expectedCount, index) => {
+      const number = index + 1;
+      cy.get(`.playerA-house${number} .count`).should('contain', expectedCount);
+    });
+  };
 });
