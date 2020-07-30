64747ef852da63791ba531a11375c95cf180f804
Rick Martínez <rickmzp@gmail.com>
2020-07-29 20:34:05 -0400

    Make integration test more readable with helpers

--- cypress/integration/game_spec.js
+++ cypress/integration/game_spec.js
@@ -2,98 +2,53 @@ describe('playing stones', () => {
   it('distributes the stones counter-clockwise', () => {
     cy.visit('./dist/index.html')
 
-    // TODO: is there a better matcher than contains?
-    cy.get('.playerA-house2 button').click();
-    boardHousesShouldLookLikeThis(
-      [4, 4, 4, 4, 4, 4],
-      [4, 0, 5, 5, 5, 5],
-    );
-    cy.get('.playerA-store').should('contain', '0');
+    play({ player: 'A', houseNum: 5 });
+    boardStateShouldBe({
+      B: { houses: [4, 4, 4, 4, 5, 5], store: 0 },
+      A: { houses: [4, 4, 4, 4, 0, 5], store: 1 },
+    });
 
-    cy.get('.playerB-house6 button').click();
-    boardHousesShouldLookLikeThis(
-      [0, 4, 4, 4, 4, 4],
-      [5, 1, 6, 5, 5, 5],
-    );
-    cy.get('.playerA-store').should('contain', '0');
+    play({ player: 'B', houseNum: 6 });
+    boardStateShouldBe({
+      B: { houses: [0, 4, 4, 4, 5, 5], store: 1 },
+      A: { houses: [5, 5, 5, 4, 0, 5], store: 1 },
+    });
   });
 
   it('repeats the turn for a player if last stone ends in store', () => {
     cy.visit('./dist/index.html')
 
-    cy.get('.playerA-house3 button').click();
-    cy.get('.playerA-store').should('contain', '1');
-    cy.get('.playerB-store').should('contain', '0');
-    boardHousesShouldLookLikeThis(
-      [4, 4, 4, 4, 4, 4],
-      [4, 4, 0, 5, 5, 5],
-    );
+    play({ player: 'A', houseNum: 3 });
+    boardStateShouldBe({
+      B: { houses: [4, 4, 4, 4, 4, 4], store: 0 },
+      A: { houses: [4, 4, 0, 5, 5, 5], store: 1 },
+    });
 
-    cy.get('.playerA-house [data-test-id=playButton]').should('have.length', 6);
-    cy.get('.playerB-house [data-test-id=playButton]').should('have.length', 0);
+    currentTurnShouldBeForPlayer('playerA');
   })
-
-  it('stores a stone when it reaches the end', () => {
-    cy.visit('./dist/index.html')
-
-    // TODO: is there a better matcher than contains?
-    cy.get('.playerA-house3 button').click();
-    boardHousesShouldLookLikeThis(
-      [4, 4, 4, 4, 4, 4],
-      [4, 4, 0, 5, 5, 5]
-    );
-    cy.get('.playerA-store').should('contain', '1');
-    cy.get('.playerB-store').should('contain', '0');
-  });
-
-  it('distributes the stones to the opposite player after reaching the end', () => {
-    cy.visit('./dist/index.html')
-
-    // TODO: is there a better matcher than contains?
-    cy.get('.playerA-house4 button').click();
-    boardHousesShouldLookLikeThis(
-      [4, 4, 4, 4, 4, 5],
-      [4, 4, 4, 0, 5, 5]
-    );
-    cy.get('.playerA-store').should('contain', '1');
-    cy.get('.playerB-store').should('contain', '0');
-  });
 
   // TODO: can we push this down into unit tests?
   it("distributes the stones when passing the opposite player's store", () => {
     cy.visit('./dist/index.html')
 
     // TODO: is there a better matcher than contains?
-    cy.get('.playerA-house1 button').click();
-    cy.get('.playerB-house1 button').click();
-    cy.get('.playerA-house2 button').click();
-    cy.get('.playerA-house3 button').click();
-    cy.get('.playerB-house2 button').click();
-    cy.get('.playerA-house4 button').click();
-    cy.get('.playerB-house3 button').click();
-    cy.get('.playerA-house5 button').click();
-    cy.get('.playerB-house6 button').click();
-    cy.get('.playerA-house6 button').click(); // play a house with >= 8 stones
+    play({ player: 'A', houseNum: 1 });
+    play({ player: 'B', houseNum: 1 });
+    play({ player: 'A', houseNum: 2 });
+    play({ player: 'A', houseNum: 3 });
+    play({ player: 'B', houseNum: 2 });
+    play({ player: 'A', houseNum: 4 });
+    play({ player: 'B', houseNum: 3 });
+    play({ player: 'A', houseNum: 5 });
+    play({ player: 'B', houseNum: 6 });
+    play({ player: 'A', houseNum: 6 }); // play a house with >= 8 stones
 
-    boardHousesShouldLookLikeThis(
-      [1, 9, 10, 2, 3, 4],
-      [4, 3, 2, 1, 1, 0]
-    );
-    cy.get('.playerA-store').should('contain', '5');
-    cy.get('.playerB-store').should('contain', '3');
+    boardStateShouldBe({
+      B: { houses: [1, 9, 10, 2, 3, 4], store: 3 },
+      A: { houses: [4, 3, 2, 1, 1, 0], store: 5 },
+    });
   });
 
-  const boardHousesShouldLookLikeThis = (playerB, playerA) => {
-    playerB.forEach((expectedCount, index) => {
-      const number = 6 - index;
-      cy.get(`.playerB-house${number} .count`).should('contain', expectedCount);
-    });
-
-    playerA.forEach((expectedCount, index) => {
-      const number = index + 1;
-      cy.get(`.playerA-house${number} .count`).should('contain', expectedCount);
-    });
-  };
 });
 
 describe('ending the game', () => {
@@ -104,12 +59,40 @@ describe('ending the game', () => {
 
     cy.get('#game-message').should('be.empty')
 
-    cy.get('.playerB-house6 [data-test-id=playButton]').click();
+    play({ player: 'B', houseNum: 6 });
+    boardStateShouldBe({
+      B: { houses: [0, 0, 0, 0, 0, 0], store: 25 },
+      A: { houses: [0, 0, 0, 0, 0, 0], store: 23 },
+    });
 
     cy.get('#game-message').should('have.text', 'Player B wins!')
-    cy.get('.playerB-store').should('have.text', '25')
-    cy.get('.playerA-store').should('have.text', '23')
 
     cy.get('[data-test-id=playButton]').should('have.length', 0)
   });
 });
+
+function play({ player, houseNum }) {
+  cy.get(
+    `.player${player}-house${houseNum} button[data-test-id=playButton]`
+  ).click();
+}
+
+function boardStateShouldBe({ A, B }) {
+  cy.get(`.playerB-store`).should('have.text', B.store);
+  B.houses.forEach((expectedCount, index) => {
+    const number = 6 - index;
+    cy.get(`.playerB-house${number} .count`).should('contain', expectedCount);
+  });
+
+  cy.get(`.playerA-store`).should('have.text', A.store);
+  A.houses.forEach((expectedCount, index) => {
+    const number = index + 1;
+    cy.get(`.playerA-house${number} .count`).should('contain', expectedCount);
+  });
+};
+
+function currentTurnShouldBeForPlayer(player) {
+  const opposingPlayer = player === 'playerA' ? 'playerB' : player;
+  cy.get(`.${player}-house [data-test-id=playButton]`).should('have.length', 6);
+  cy.get(`.${opposingPlayer}-house [data-test-id=playButton]`).should('have.length', 0);
+}
